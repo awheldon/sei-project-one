6ec675ed8900570c3d51ea64b2749c82e265fb10
Alex Wheldon <a.wheldon@gmail.com>
2020-04-13 21:00:39 +0100

    all rotation data finalised. rotation function for all shapes built

--- scripts/app.js
+++ scripts/app.js
@@ -23,7 +23,6 @@ function init() {
   function createGrid() {
     for (let i = 0; i < width * height; i++) {
       const cell = document.createElement('div')
-      cell.textContent = i
       grid.appendChild(cell)
       cells.push(cell)
     }
@@ -45,23 +44,43 @@ function init() {
   }
   const lShape = {
     name: 'l',
-    startingPosition: [2, 10, 11, 12]
+    startingPosition: [2, 10, 11, 12],
+    rotation90: [20, -9, 0, 9],
+    rotation180: [-2, 11, 0, -11],
+    rotation270: [-20, 9, 0, -9],
+    rotation360: [2, -11, 0, 11]
   }
   const sShape = {
     name: 's',
-    startingPosition: [1, 2, 10, 11]
+    startingPosition: [1, 2, 10, 11],
+    rotation90: [11, 20, -9, 0],
+    rotation180: [9, -2, 11, 0],
+    rotation270: [-11, -20, 9, 0],
+    rotation360: [-9, 2, -11, 0]
   }
   const tShape = {
     name: 't',
-    startingPosition: [1, 10, 11, 12]
+    startingPosition: [1, 10, 11, 12],
+    rotation90: [11, -9, 0, 9],
+    rotation180: [9, 11, 0, -11],
+    rotation270: [-11, 9, 0, -9],
+    rotation360: [-9, -11, 0, 11]
   }
   const iShape = {
     name: 'i',
-    startingPosition: [10, 11, 12, 13]
+    startingPosition: [10, 11, 12, 13],
+    rotation90: [-8, 1, 10, 19],
+    rotation180: [21, 10, -1, -12],
+    rotation270: [8, -1, -10, -19],
+    rotation360: [-21, -10, 1, 12]
   }
   const zShape = {
     name: 'z',
-    startingPosition: [0, 1, 11, 12]
+    startingPosition: [0, 1, 11, 12],
+    rotation90: [2, 11, 0, 9],
+    rotation180: [20, 9, 0, -11],
+    rotation270: [-2, -11, 0, -9],
+    rotation360: [-20, -9, 0, 11]
   }
 
   const tetriminos = [oShape, jShape, lShape, sShape, tShape, iShape, zShape]
@@ -70,7 +89,7 @@ function init() {
 
   function createTetrimino() {
     let nextShape = []
-    nextShape = tetriminos[Math.floor(Math.random() * (tetriminos.length - 1))]
+    nextShape = tetriminos[Math.floor(Math.random() * (tetriminos.length))]
     nextShape.startingPosition.forEach(cell => {
       cells[cell].classList.add('playertetrimino')
       playerTetriminoPosition.push(cell)
@@ -137,7 +156,112 @@ function init() {
             playerTetriminoPosition[i] = playerTetriminoPosition[i] + jShape.rotation360[i]
           }
           currentTetrimino = 'j'
-        }                                    
+        }
+        if (currentTetrimino === 'l') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + lShape.rotation90[i]
+          }
+          currentTetrimino = 'l90'
+        } else if (currentTetrimino === 'l90') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + lShape.rotation180[i]
+          }
+          currentTetrimino = 'l180'
+        } else if (currentTetrimino === 'l180') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + lShape.rotation270[i]
+          }
+          currentTetrimino = 'l270'
+        } else if (currentTetrimino === 'l270') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + lShape.rotation360[i]
+          }
+          currentTetrimino = 'l'
+        }
+        if (currentTetrimino === 's') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + sShape.rotation90[i]
+          }
+          currentTetrimino = 's90'
+        } else if (currentTetrimino === 's90') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + sShape.rotation180[i]
+          }
+          currentTetrimino = 's180'
+        } else if (currentTetrimino === 's180') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + sShape.rotation270[i]
+          }
+          currentTetrimino = 's270'
+        } else if (currentTetrimino === 's270') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + sShape.rotation360[i]
+          }
+          currentTetrimino = 's'
+        }
+        if (currentTetrimino === 't') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + tShape.rotation90[i]
+          }
+          currentTetrimino = 't90'
+        } else if (currentTetrimino === 't90') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + tShape.rotation180[i]
+          }
+          currentTetrimino = 't180'
+        } else if (currentTetrimino === 't180') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + tShape.rotation270[i]
+          }
+          currentTetrimino = 't270'
+        } else if (currentTetrimino === 't270') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + tShape.rotation360[i]
+          }
+          currentTetrimino = 't'
+        }
+        if (currentTetrimino === 'i') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + iShape.rotation90[i]
+          }
+          currentTetrimino = 'i90'
+        } else if (currentTetrimino === 'i90') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + iShape.rotation180[i]
+          }
+          currentTetrimino = 'i180'
+        } else if (currentTetrimino === 'i180') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + iShape.rotation270[i]
+          }
+          currentTetrimino = 'i270'
+        } else if (currentTetrimino === 'i270') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + iShape.rotation360[i]
+          }
+          currentTetrimino = 'i'
+        }
+        if (currentTetrimino === 'z') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + zShape.rotation90[i]
+          }
+          currentTetrimino = 'z90'
+        } else if (currentTetrimino === 'z90') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + zShape.rotation180[i]
+          }
+          currentTetrimino = 'z180'
+        } else if (currentTetrimino === 'z180') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + zShape.rotation270[i]
+          }
+          currentTetrimino = 'z270'
+        } else if (currentTetrimino === 'z270') {
+          for (let i = 0; i < playerTetriminoPosition.length; i++) {
+            playerTetriminoPosition[i] = playerTetriminoPosition[i] + zShape.rotation360[i]
+          }
+          currentTetrimino = 'z'
+        }                                                                
         break
       default:
         console.log('invalid key do nothing')
